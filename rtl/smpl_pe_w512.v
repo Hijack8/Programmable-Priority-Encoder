e2a4a3d622f77808a4f5500765a4c5fbd6363bf3
YunguangLi <yunguangli1@outlook.com>
2024-12-27 07:06:34 +0000

    :fire: Remove the useless file.

--- rtl/smpl_pe_w512.v
+++
@@ -1,9 +0,0 @@
-module smpl_pe_w512(
-    input              [ 511: 0]        Req                        ,
-    output             [ 511: 0]        Gnt                        ,
-    output                              valid                       
-);
-    assign Gnt = Req & (~Req + 1);
-    assign valid = |Req;
-                                                                   
-endmodule
